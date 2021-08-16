af20a0ccf857b797571814234849575593065557
Mathew <mveale8@gmail.com>
2021-08-16 15:21:10 +1200

    Dependencies added

--- TextEditor/src/main/java/application/App.java
+++ TextEditor/src/main/java/application/App.java
@@ -1,13 +1,42 @@
 package application;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import javafx.scene.layout.VBox;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws IOException 
+	{
+		// Create the FXMLLoader 
+		FXMLLoader loader = new FXMLLoader();
+		// Path to the FXML File
+		String fxmlDocPath = "src/main/resources/fxml/TextEditorLayout.fxml";
+		FileInputStream fxmlStream = new FileInputStream(fxmlDocPath);
+		
+		// Create the Pane and all Details
+		VBox root = new VBox();
+		root = (VBox) loader.load(fxmlStream);	
+		
+		// Create the Scene
+		Scene scene = new Scene(root);
+		// Set the Scene to the Stage
+		stage.setScene(scene);
+		// Set the Title to the Stage
+		stage.setTitle("Text Editor");
+		// Display the Stage
+		stage.show();
+	}
+
 }
--- TextEditor/src/test/java/application/AppTest.java
+++ TextEditor/src/test/java/application/AppTest.java
@@ -1,38 +1,25 @@
 package application;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    @Test
+    public void appTest()
     {
-        super( testName );
+        assertEquals(2, 2);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
+
