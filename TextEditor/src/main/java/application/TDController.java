218368d37e56e99899f416cf8d92ca9757936048
zjack8 <61671353+zjack8@users.noreply.github.com>
2021-09-02 17:48:48 +1200

    Added TDController.java

---
+++ TextEditor/src/main/java/application/TDController.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TDController {
+	
+	@FXML
+	private Label TD;
+	
+	// Add a public no-args constructor
+    public TDController() {
+    }
+
+    @FXML
+    private void initialize() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+        Date date = new Date();  
+		TD.setText(formatter.format(date));
+    }
+}
